483c240bdb6713bf4ba7df99711e3663db808044
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-08 11:54:16 -0300

    refactor: font size button

--- src/components/ui/button/styled.ts
+++ src/components/ui/button/styled.ts
@@ -76,7 +76,7 @@ function disabledBackgroundBorder(color: string, backgroundColor: string) {
 export const Button = styled.button<StyledButtonProps>`
 	font-weight: ${({ theme }) => theme.typography.fontWeight.medium};
 	font-family: ${({ theme }) => theme.typography.fontFamily.regular};
-	font-size: ${({ theme }) => theme.typography.fontSize.lg};
+	font-size: ${({ theme }) => theme.typography.fontSize.md};
 	line-height: ${({ theme }) => theme.typography.lineHeight.base};
 	letter-spacing: ${({ theme }) => theme.spacing.px};
 	border-radius: ${({ theme }) => theme.border.borderRadius.sm};
