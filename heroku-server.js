6a4a227064e2f9b827e2c51556c7e8b4f720466b
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-09 01:26:45 -0300

    refactor: remove static json file and add routes do server heroku

--- heroku-server.js
+++ heroku-server.js
@@ -6,6 +6,12 @@ const app = express()
 
 app.use('/', express.static(resolve(__dirname, './build')))
 
+app.use('/users', express.static(resolve(__dirname, './build')))
+
+app.use('/users/form', express.static(resolve(__dirname, './build')))
+
+app.use('/users/detail/:id', express.static(resolve(__dirname, './build')))
+
 app.listen(process.env.PORT || 3000, () =>
 	console.log(`server ir running on port ${process.env.PORT || 3000}`)
 )
